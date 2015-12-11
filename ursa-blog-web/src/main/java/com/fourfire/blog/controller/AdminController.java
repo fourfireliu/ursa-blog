e06ed9efca8e604f019af4fdb77435ee3f0274ad
liuyi938 <liuyi@jk.cn>
2015-12-11 17:59:24 +0800

    20151211 1759 update

--- ursa-blog-web/src/main/java/com/fourfire/blog/controller/AdminController.java
+++ ursa-blog-web/src/main/java/com/fourfire/blog/controller/AdminController.java
@@ -123,35 +123,10 @@ public class AdminController {
 		long begin = System.currentTimeMillis();
 		logger.info("index method begin");
 		
-		try {
-			List<TypeInfoVO> typeInfoVOs = typeInfoManager.getAllTypeInfos();
-			if (typeInfoVOs == null) {
-				logger.error("index method get type list null");
-			} else {
-				//置顶文章类型列表
-				modelMap.put("typeInfos", typeInfoVOs);
-				if (typeInfoVOs.get(0) != null) {
-					int defaultTypeId = typeInfoVOs.get(0).getId();
-					PageResult<ArticleInfoVO> articleInfoPageResult = articleInfoManager.pageQueryArticles(0, BlogConstant.DEFAULT_ARTICLE_LIST_SIZE, defaultTypeId, null, ArticleInfoType.SHORT_CONTENT);
-					if (articleInfoPageResult != null && articleInfoPageResult.isSuccess() && articleInfoPageResult.getPageResult() != null) {
-						//置顶文章 按评论数排序
-						modelMap.put("topArticles", articleInfoPageResult.getPageResult());
-					}
-				}
-			}
-			
-			//获取最新文章列表
-			PageResult<ArticleInfoVO> newArticleInfoPageResult = articleInfoManager.pageQueryArticles(0, BlogConstant.DEFAULT_ARTICLE_LIST_SIZE, -1, "modify_gmt_date desc", ArticleInfoType.NO_CONTENT);
-			if (newArticleInfoPageResult != null && newArticleInfoPageResult.isSuccess() && newArticleInfoPageResult.getPageResult() != null) {
-				modelMap.put("newArticles", newArticleInfoPageResult.getPageResult());
-			}
-			
-		} catch (Exception e) {
-			logger.error("unknown exception", e);
-		}
+		
 		
 		long end = System.currentTimeMillis();
 		logger.info("index method end, cost time=" + (end - begin) + "ms");
-		return "index";
+		return "page/writearticle";
 	}
 }
