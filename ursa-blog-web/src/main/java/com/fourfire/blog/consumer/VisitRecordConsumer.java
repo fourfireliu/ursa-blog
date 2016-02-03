9549b0e2b579431408427e6d529d58e4fa9e2cc3
liuyi938 <liuyi@jk.cn>
2016-02-03 17:59:27 +0800

    201602031759 update

---
+++ ursa-blog-web/src/main/java/com/fourfire/blog/consumer/VisitRecordConsumer.java
@@ -0,0 +1,13 @@
+package com.fourfire.blog.consumer;
+
+/**
+ * 用于处理用户访问统计的线程
+ * 
+ * @author liuyi
+ * @date 2016-02-03
+ */
+public class VisitRecordConsumer extends Thread {
+	public VisitRecordConsumer() {
+		
+	}
+}
